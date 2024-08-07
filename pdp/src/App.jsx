f752675972873e6e2b0a7e21ac156d5ee562facc
elbachir-salik <salik.elbachir@gmail.com>
2024-08-07 10:28:24 +0100

    Asynchronous loading the header

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.scss";
 
-import Header from "home/Header";
+const Header =
+    React.lazy(() => import("home/Header"));
 import Footer from "home/Footer";
 
 
-const App = () => (
-    <div className="text-3xl mx-auto max-w-6xl">
-        <Header />
-        <div className="my-10">PDP Page Content</div>
-        <Footer/>
-    </div>
-);
+const App = () => {
+    const [showHeader, setShowHeader] = useState(false);
+    return (
+        <div className="text-3xl mx-auto max-w-6xl">
+            {showHeader && <Suspense fallback={<div>Loading</div>}>
+                <Header />
+            </Suspense>}
+            <button className={"text-3xl p-5"}
+            onClick={() => setShowHeader(true)}>Show the Header</button>
+            <div className="my-10">PDP Page Content</div>
+            <Footer/>
+        </div>
+    );
+}
 const rootElement = document.getElementById("app")
 if (!rootElement) throw new Error("Failed to find the root element")
 
