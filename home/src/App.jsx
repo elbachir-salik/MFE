025bab7fc0ee5238b54a35fc9966255546837fba
elbachir-salik <salik.elbachir@gmail.com>
2024-08-07 09:09:51 +0100

    Added a Header and a Footer and expoese them

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Header from "./Header";
+import Footer from "./Footer";
+
 import "./index.scss";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home Page Content</div>
+    <Footer/>
   </div>
 );
 const rootElement = document.getElementById("app")
