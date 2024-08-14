80cb649fea1e4b0384cceb91663621d0650db1c6
elbachir-salik <salik.elbachir@gmail.com>
2024-08-14 14:13:58 +0100

    Adding the header and footer to pdp

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,27 +1,19 @@
-import React, { Suspense, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.scss";
 
-const Header =
-    React.lazy(() => import("home/Header"));
+import Header from "home/Header";
 import Footer from "home/Footer";
 
 
-const App = () => {
-    const [showHeader, setShowHeader] = useState(false);
-    return (
-        <div className="text-3xl mx-auto max-w-6xl">
-            {showHeader && <Suspense fallback={<div>Loading</div>}>
-                <Header />
-            </Suspense>}
-            <button className={"text-3xl p-5"}
-            onClick={() => setShowHeader(true)}>Show the Header</button>
-            <div className="my-10">PDP Page Content</div>
-            <Footer/>
-        </div>
-    );
-}
+const App = () => (
+    <div className="text-3xl mx-auto max-w-6xl">
+        <Header />
+        <div className="my-10">PDP Page Content</div>
+        <Footer/>
+    </div>
+);
 const rootElement = document.getElementById("app")
 if (!rootElement) throw new Error("Failed to find the root element")
 
