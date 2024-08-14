a31dc214811fea7dc61bdb8a108e82f497ce285f
elbachir-salik <salik.elbachir@gmail.com>
2024-08-14 14:52:43 +0100

    Adding a safecomponent to catch errors

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -4,6 +4,6 @@ export default function Header() {
         return(
             <div
                 className="p-5 bg-blue-500 text-white text-3xl font-bold">
-                    Fidget Spinner World </div>
+                    Fidget Spinner World  </div>
         )
 }
