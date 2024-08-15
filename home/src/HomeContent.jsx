316637fae0463b9b163c17ba89f76e5c2670a576
elbachir-salik <salik.elbachir@gmail.com>
2024-08-15 19:27:13 +0100

    Added the product name, price and the description in the homecontent component

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -10,10 +10,17 @@ export default function HomeContent() {
     }, []);
 
     return (
-        <div className="grid-cols-4 gap-5">
+        <div className="grid grid-cols-4 gap-5">
             {products.map((product)=> (
                 <div key={product.id}>
                     <img src={product.image} alt={product.name} />
+                    <div className='flex'>
+                        <div className="flex-grow font-bold">
+                            <a>{product.name}</a>
+                        </div>
+                        <div className="flex-end">{currency.format(product.price)}</div>
+                    </div>
+                    <div className="text-sm mt-4">{product.description}</div>
                 </div>
                 )
             )}
